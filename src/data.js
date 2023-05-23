82bbe87523d9fedc4b2cbfb9cbb4fdb9f8a5ae27
Ing.w <william.esteban@campusucc.edu.com>
2023-05-23 18:25:01 -0500

    Sdfsd

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const imgs = [
+	{
+		id: 1,
+		img: '/img/g1.png',
+		alt: 'g1',
+	},
+	{
+		id: 2,
+		img: '/img/g2.png',
+		alt: 'g2',
+	},
+	{
+		id: 3,
+		img: '/img/g3.png',
+		alt: 'g3',
+	},
+	{
+		id: 4,
+		img: '/img/g4.png',
+		alt: 'g4',
+	},
+	{
+		id: 5,
+		img: '/img/g5.png',
+		alt: 'g5',
+	},
+	{
+		id: 6,
+		img: '/img/g6.png',
+		alt: 'g6',
+	},
+	{
+		id: 7,
+		img: '/img/g7.png',
+		alt: 'g7',
+	},
+	{
+		id: 8,
+		img: '/img/g8.png',
+		alt: 'g8',
+	},
+	{
+		id: 9,
+		img: '/img/g9.png',
+		alt: 'g9',
+	},
+	{ 	id: 10,
+	 	img: '/img/g10.png',
+	 	alt: 'g10' 
+	},
+];
